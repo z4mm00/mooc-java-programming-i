c42bd8dfeafe738c60f5ad9864859a03e186e607
z4mm00 <z4mm00@hotmail.com>
2021-01-19 19:29:12 -0600

    New round of completed exercises

--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,13 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                return;
+            }
+            String[] words = input.split(" ");
+            System.out.println(words[words.length - 1]);
+        }
     }
 }
